dbf907b2710acfa5c4a07884cf44db948758ce22
CharissaHollister <charissa.hollister@diasorin.com>
2022-07-08 21:54:02 -0500

    add handlebars

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
 const express = require("express");
-const routes = require("./routes");
+const routes = require("./controllers");
 const sequelize = require("./config/connection");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
 // turn on routes
 app.use(routes);
 
+//code to set up Handlebars.js as your app's template engine of choice
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({});
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
 // turn on connection to db and server
-////note: change force to true to reset tables
+//!note: change force to true to reset tables
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
 });
