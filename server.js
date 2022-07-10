27cb855c9a3a1ca41e91273f16d334eb1e3bd867
CharissaHollister <charissa.hollister@diasorin.com>
2022-07-10 12:20:40 -0500

    helper tests

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
 const path = require("path");
+const helpers = require("./utils/helpers");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -32,7 +33,7 @@ app.use(routes);
 
 //code to set up Handlebars.js as your app's template engine of choice
 const exphbs = require("express-handlebars");
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
